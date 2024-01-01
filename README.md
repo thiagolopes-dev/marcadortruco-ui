bef894662e6da126a37078e89828e7c5c1af18bb
Thiago Lopes <thiagoolopezz@gmail.com>
2024-01-01 15:28:47 -0300

    Melhorias PWA

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-#Aplicativo Marcador de Truco
-Desenvolvido por Thiago Lopes
+
+# Aplicativo Trucou
+
+Aplicativo para marcar jogo de Truco, desenvolvido com Angular + PrimeNG.
+
+## Funcionalidades
+
+- Aplicativo PWA, instalável em qualquer dispositivo
+- Funciona Offline
+- Sem anúncios, ou qualquer captura de dados do usuário
+- Funcionalidades Inteligentes, como pontuação +12, soma automaticamente pontos de partidas vencidas.
+
+## Autores
+
+- [@thiagolopesdev](https://www.instagram.com/thiagolopes.dev/)
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -6,6 +6,7 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeNGModule } from './primeng.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,13 @@ import { PrimeNGModule } from './primeng.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    PrimeNGModule
+    PrimeNGModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     ConfirmationService,
